cbe28b50b382a4dd37fa95581956b395a88d45b3
jakeobsen <morten@jakeobsen.com>
2021-07-28 23:24:07 +0100

    Bugfix: Fix import problem caused by ModuleNotFoundError
    When running the package from an IDE it cannot import the cli module when the import has .cli on it. But when running this with python -m it cannot import the cli module when the dot is missing.
    
    This fix will ensure both can run.

--- src/autoconfsh/__main__.py
+++ src/autoconfsh/__main__.py
@@ -1,7 +1,14 @@
-#from cli import cli_entrypoint
+import logging
+from sys import exit
 
-print(__name__)
+try:
+    # IDE support
+    logging.debug("Attempting to import cli.cli_entrypoint")
+    from cli import cli_entrypoint
+except ModuleNotFoundError as e:
+    # python -m support
+    logging.debug("Caught import error, attempting to import .cli.cli_entrypoint")
+    from .cli import cli_entrypoint
 
 if __name__ == '__main__':
-    print("WOOOP!")
-#    cli_entrypoint()
+    exit(cli_entrypoint())
