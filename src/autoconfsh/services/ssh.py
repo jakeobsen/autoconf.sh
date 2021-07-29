1bbf67d7b0de5ac3a1c700bf8be2a96d2a43eb0d
jakeobsen <morten@jakeobsen.com>
2021-07-29 01:30:00 +0100

    Add initial ssh service class

---
+++ src/autoconfsh/services/ssh.py
@@ -0,0 +1,17 @@
+from .service import Service
+
+class SSH(Service):
+    """
+    .. warning::
+       This class is not fully implemented yet.
+    """
+
+    def __init__(self):
+        super().__init__("ssh")
+
+    def start(self):
+        """
+        This is a test
+        :return:
+        """
+        pass
